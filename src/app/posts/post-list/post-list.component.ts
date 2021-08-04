91df3b3d521e96d6d2530ebe4df4ef38496637c6
Alexis Coria <coria.alexis94@gmail.com>
2021-08-03 23:58:14 -0300

    post-list-component

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-post-list',
+    templateUrl:'./post-list.component.html',
+    styleUrls:['./post-list.component.css']
+}
+)
+export class PostListComponent{
+    posts = [
+        //{title: "Primer titulo",content:"Este es el primer contenido"},
+        //{title: "Segundo titulo",content:"Este es el segundo contenido"},
+        //{title: "Tercer titulo",content:"Este es el  tercer contenido"}
+    ]
+}
